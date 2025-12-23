db5d0053678714887e7d360f76b0a2a1e0737401
Faye8saud <fayii8866@gmail.com>
2025-12-23 14:58:54 +0300

    Initial Commit

---
+++ MovieIsMe_Team1_Afternoon/MovieIsMe_Team1_AfternoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieIsMe_Team1_AfternoonApp.swift
+//  MovieIsMe_Team1_Afternoon
+//
+//  Created by Fay  on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieIsMe_Team1_AfternoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieIsMe_Team1_AfternoonTests/MovieIsMe_Team1_AfternoonTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieIsMe_Team1_AfternoonTests.swift
+//  MovieIsMe_Team1_AfternoonTests
+//
+//  Created by Fay  on 23/12/2025.
+//
+
+import Testing
+@testable import MovieIsMe_Team1_Afternoon
+
+struct MovieIsMe_Team1_AfternoonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
