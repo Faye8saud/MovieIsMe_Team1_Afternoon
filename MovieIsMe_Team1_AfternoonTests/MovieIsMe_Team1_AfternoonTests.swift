cd4889432bbc4f4c8aa3997b9ccc1d6959f0956e
wasan <ameera19x@gmail.com>
2025-12-29 16:56:22 +0300

    wasan last

--- MovieIsMe_Team1_AfternoonTests/MovieIsMe_Team1_AfternoonTests.swift
+++ MovieIsMe_Team1_AfternoonTests/MovieIsMe_Team1_AfternoonTests.swift
@@ -1,17 +1,17 @@
+////
+////  MovieIsMe_Team1_AfternoonTests.swift
+////  MovieIsMe_Team1_AfternoonTests
+////
+////  Created by Fay  on 23/12/2025.
+////
 //
-//  MovieIsMe_Team1_AfternoonTests.swift
-//  MovieIsMe_Team1_AfternoonTests
+//import Testing
+//@testable import MovieIsMe_Team1_Afternoon
 //
-//  Created by Fay  on 23/12/2025.
+//struct MovieIsMe_Team1_AfternoonTests {
 //
-
-import Testing
-@testable import MovieIsMe_Team1_Afternoon
-
-struct MovieIsMe_Team1_AfternoonTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+//
+//}
